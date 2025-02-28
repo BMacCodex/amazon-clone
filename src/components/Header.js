facb8b809661992dcc53f9153c121580665f02a6
Brian Mcloughlin <brian.mcloughlin92@gmail.com>
2025-02-27 22:26:56 -0500

    Added custom css theme and it works
    Applied tailwinds custom colour theme

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      {/* Top Nav */}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div>
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+            alt="Amazon Logo"
+          />
+        </div>
+      </div>
+
+      {/* Bottom Nav */}
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
