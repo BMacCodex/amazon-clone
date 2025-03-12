d0aba2a0879c577198b1501a114058292b604a3d
Brian Mcloughlin <brian.mcloughlin92@gmail.com>
2025-03-12 12:21:44 -0400

    Changing img tags to Image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import ReactDOM from "react-dom";
+import Image from "next/image";
 
 function Banner() {
   return (
@@ -16,13 +17,31 @@ function Banner() {
         interval={5000}
       >
         <div>
-          <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
+          <Image
+            loading="lazy"
+            src="https://links.papareact.com/gi1"
+            alt=""
+            width="2000"
+            height="2000"
+          />
         </div>
         <div>
-          <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+          <Image
+            loading="lazy"
+            src="https://links.papareact.com/6ff"
+            alt=""
+            width="2000"
+            height="2000"
+          />
         </div>
         <div>
-          <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+          <Image
+            loading="lazy"
+            src="https://links.papareact.com/7ma"
+            alt=""
+            width="2000"
+            height="2000"
+          />
         </div>
       </Carousel>
     </div>
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Product from "./Product";
+import Image from "next/image";
 
 function ProductFeed({ products }) {
   return (
